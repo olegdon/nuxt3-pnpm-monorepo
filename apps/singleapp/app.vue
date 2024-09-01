ec46131306fc7531293b813137a1acb7aeddd46a
Oleg Don <oleg.don@checkout-charlie.com>
2024-09-01 14:35:53 +0200

    init singleapp

--- apps/singleapp/app.vue
+++ apps/singleapp/app.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <NuxtRouteAnnouncer />
+    <h1>Single APP</h1>
     <NuxtWelcome />
   </div>
 </template>
--- apps/singleapp/nuxt.config.ts
+++ apps/singleapp/nuxt.config.ts
@@ -1,5 +1,75 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+// https://nuxt.com/docs/getting-started/configuration
+// https://nuxt.com/docs/api/nuxt-config
+
+import tailwindConfig from '@olegdon/design/configs/tailwind.singleapp.config'
+
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
+  imports: {
+    dirs: [
+      'stores',
+      '../../packages/services/composables',
+      '../../packages/services/utils',
+    ],
+  },
+  modules: [
+    [
+      '@pinia/nuxt',
+      {
+        autoImports: ['defineStore', 'acceptHMRUpdate'],
+      },
+    ],
+    '@nuxtjs/i18n',
+    '@vueuse/nuxt',
+    '@vue-macros/nuxt',
+    'nuxt-schema-org',
+    [
+      '@olegdon/design/nuxt.ts',
+      { app: 'singleapp' },
+    ],
+    [
+      '@olegdon/modules/nuxt-app-module-config/module.ts',
+      {
+        app: 'singleapp',
+        tailwind: tailwindConfig,
+        fonts: [
+          {
+            name: 'Niramit',
+            src: './node_modules/@olegdon/design/public/assets/fonts/niramit/*.woff2',
+            dst: '@olegdon/design/public/assets/fonts/niramit/',
+            type: 'sans',
+          },
+        ],
+        image: {},
+      },
+    ],
+  ],
+  typescript: {
+    shim: false,
+  },
+  runtimeConfig: {
+    public: {
+      app: {},
+    },
+  },
+  i18n: {
+    detectBrowserLanguage: false,
+    customRoutes: 'page',
+    defaultLocale: 'en-US',
+    baseUrl: 'http://example.com',
+    lazy: true,
+    langDir: '../../packages/translations',
+    vueI18n: '../../packages/translations/vue-i18n.ts',
+    locales: [
+      {
+        code: 'en-US',
+        files: [
+          'en-US.ts',
+          'singleapp/en-US.ts',
+        ],
+        language: 'en-US',
+      },
+    ],
+  },
 })
