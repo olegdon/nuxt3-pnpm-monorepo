ed8be608fd4d6680aa5f458e6ed932a20c3a0bed
Oleg Don <oleg.don@checkout-charlie.com>
2024-09-29 11:36:03 +0200

    prep monorepo

--- apps/webapp/nuxt.config.ts
+++ apps/webapp/nuxt.config.ts
@@ -103,8 +103,8 @@ export default defineNuxtConfig({
     ],
   },
   gtag: {
-    // eslint-disable-next-line node/prefer-global/process
-    enabled: process.env.NODE_ENV === 'production',
+
+    enabled: true,
     // eslint-disable-next-line node/prefer-global/process
     id: process.env.GOOGLE_ANALYTICS_ID,
   },
